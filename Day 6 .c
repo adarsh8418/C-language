e68a98fb3df5800acdb2707631191b7f59024d57
adarsh8418 <8418dubey@gmail.com>
2024-07-01 11:49:44 +0530

    Add files via upload

---
+++ Day 6 .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y;
+	printf("Enter First Number :");
+	scanf("%d",&x);
+	printf("Enter Second Number :");
+	scanf("%d",&y);
+	(x<y)?printf("x is minimum"):printf("y is minimum");
+}
